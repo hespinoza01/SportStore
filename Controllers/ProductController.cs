d01fe969b3a3b0221af278a08859039af2e07c47
Harold Espinoza <haroldesptru@gmail.com>
2019-05-10 21:12:55 -0600

    Añadido código base para habilitar paginación en la lista de productos, se insertaron más productos de ejemplos para visualizar mejor la paginación

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportStore.Models;
+using System.Linq;
 
 namespace SportStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductStorage storage;
+        public int PageSize = 4;
 
         public ProductController(IProductStorage _storage)
         {
             this.storage = _storage;
         }
 
-        public ViewResult List() => this.View(this.storage.Products);
+        public ViewResult List(int productPage = 1) => this.View(
+            this.storage.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * this.PageSize)
+                .Take(this.PageSize)
+        );
     }
 }
